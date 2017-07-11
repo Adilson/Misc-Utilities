3b20c5f2cf0076d34663697be1442c944580d1fc
Adilson Jr <adilson.smem@gmail.com>
2017-07-11 02:34:40 -0300

    Solucao iniciada

--- solve-bbc-puzzle3/solution.js
+++ solve-bbc-puzzle3/solution.js
@@ -2,7 +2,7 @@
 var dict = new Array(28);
 dict[0] = ' ';
 for (var i=65; i<65+26; i++)
-	dict[i-64] = string.fromCharCode(i);
+	dict[i-64] = String.fromCharCode(i);
 dict[27] = '-';
 
 var arr = new Array(70);
@@ -33,3 +33,113 @@ vConds[10] = /N(XN|ZB|CA|FS){2}/g;
 vConds[11] = /[XHDJ]R[MZVIJ]EC/g;
 vConds[12] = /X?W(\sE|OS|PE){2}/g;
 vConds[13] = /[VIMZJ]{3}\-\s/g;
+
+
+function getRow(i)
+{
+	var ret = arr.slice(i*14, (i+1)*14);
+	return ret.map(x=>dict[x]).join('');
+}
+
+function getCol(j)
+{
+	return [0,1,2,3,4].map(x=>dict[arr[x*14+j]]).join('');
+}
+
+function incrCol(j)
+{
+	arr[56+j] += 1;
+	for (var i=4; i>0; i--)
+	{
+		if (arr[(14*i)+j] >= dict.length)
+		{
+			arr[(14*i)+j] = 0;
+			arr[(14*i)+j-14] += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+var colSolutions = vConds.map(x=>[]);
+for (var j=0; j<vConds.length; j++)
+{
+	for (var r=0; r<28**5-1; r++)
+	{
+		var col = getCol(j);
+		if (vConds[j].test(col)) 
+			colSolutions[j].push(col);
+		incrCol(j);
+	}
+	console.log(colSolutions[j].length);
+}
+
+
+//A esta altura colSolutions é um array, onde cada posição é uma lista de colunas que atendem as contraints verticais
+
+//TODO:
+// Opcão 1: utilizar para as colunas com muitas opções a opção dos caracteres
+// Opção 2: quebrar as contraints horizontais em constraints menores, que sirvam para eliminar as primeiras colunas
+
+
+var arrMax = colSolutions.map(x=>x.length);
+var rMax = arrMax.reduce((cur,prev)=>prev*cur,1);
+console.log('Total ' + rMax);
+
+var endSolution = [];
+
+var arrindexes = colSolutions.map(x=>0);
+for (var r1 = 0; r1<rMax-1; r1++)
+{
+	for (var j=0; j<arrindexes.length-1; j++)
+	{
+		if (arrindexes[j] >= arrMax[j])
+		{
+			arrindexes[j] = 0;
+			arrindexes[j+1] += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	var notSolution = false;
+	for (var i=0; i<hConds.length; i++)
+	{
+		//console.log(arrindexes.join(','));
+		var row = colSolutions.map((x,i2)=>x[arrindexes[i2]][i]).join('');
+		if (!hConds[i].test(row))
+		{
+			notSolution = true;
+			break;
+		}
+	}
+	if (notSolution)
+	{
+		arrindexes[0] += 1;
+		continue;
+	}
+	endSolution.push(arrindexes);
+	break;
+}
+
+if (endSolution.length > 0)
+{
+	var solutionIndexes = endSolution[0];
+	for (var i=0; i<5; i++)
+	{
+		var row = colSolutions.map((x,i2)=>x[solutionIndexes[i2]][i]).join('');
+		console.log(row);
+	}
+}
+
+
+console.log(getRow(0));
+console.log(getRow(1));
+console.log(getRow(2));
+console.log(getRow(3));
+console.log(getRow(4));
+//console.log(getCol(0));
+//console.log(getCol(3));
