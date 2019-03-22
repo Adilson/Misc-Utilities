3a7970827b4130b03066f9daf54af546806283a6
Adilson <adilson.smem@gmail.com>
2019-03-22 16:23:54 -0300

    - resolvido

---
+++ web-debugger/dist/app.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class App {
+    constructor() {
+        this.app = express_1.default();
+        this.app.use(express_1.default.json());
+        this.mountRoutes();
+    }
+    mountRoutes() {
+        const router = express_1.default.Router();
+        router.get('*', (req, res) => {
+            res.json({
+                qs: req.query,
+                url: req.url,
+                messsage: "ok"
+            });
+        });
+        router.post('*', (req, res) => {
+            console.log({
+                url: req.url,
+                qs: req.query,
+                body: req.body
+            });
+            res.json({ message: 'ok' });
+        });
+        this.app.use('/', router);
+    }
+}
+var app = new App().app;
+exports.default = app;
+//# sourceMappingURL=app.js.map
---
+++ web-debugger/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+
+class App {
+    public app: express.Application;
+
+    constructor () {
+        this.app = express();
+        this.app.use(express.json());
+        this.mountRoutes();
+    }
+
+    private mountRoutes (): void {
+        const router = express.Router();
+        router.get('*', (req, res) => {
+            res.json({
+                qs: req.query,
+                url: req.url,
+                messsage: "ok"
+            });
+        });
+        router.post('*', (req, res) => {
+            console.log({
+                url: req.url,
+                qs: req.query,
+                body: req.body
+            });
+            res.json({message:'ok'});
+        });
+        this.app.use('/', router);
+    }
+}
+
+var app = new App().app;
+export default app;
