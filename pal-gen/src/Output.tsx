77aa8908a6549976f5983e37ec32ff07afc54fa7
Adilson <adilson.smem@gmail.com>
2019-09-18 15:12:26 -0300

    Controle do S e V values

--- pal-gen/src/Output.tsx
+++ pal-gen/src/Output.tsx
@@ -5,11 +5,23 @@ export class Output extends React.PureComponent<OutputState>
     private hsvToRgb = (h:number,s:number,v:number) => ((c,x)=> ((m,rgb,f)=>rgb.map(k=>f(k+m)))(v-c,h<60?[c,x,0]:h<120?[x,c,0]:h<180?[0,c,x]:h<240?[0,x,c]:h<300?[x,0,c]:[c,0,x],(l:number)=>Math.round(l*255)))(v*s, v*s*(1-Math.abs((h/60)%2 - 1)));
     getColorDegress(index:number, total:number)
     {
-        return Math.floor(360*index/total);
+        return Math.floor(360*2*Math.floor(index/2)/total);
+    }
+    getColorS(index:number, total:number)
+    {
+        return [0.2, 0.5, 1, 0.5, 1][index%5];
+    }
+    getColorV(index:number, total:number)
+    {
+        return [1,0.8,0.9][index%3];
     }
     renderColors() {
         return <ul>
-            {Array.of(...new Array(this.props.numberOfColors)).map((x,i)=>this.renderColor(this.getColorDegress(i,this.props.numberOfColors)))}
+            {Array.of(...new Array(this.props.numberOfColors)).map((x,i)=>this.renderColor(
+                this.getColorDegress(i,this.props.numberOfColors),
+                this.getColorS(i,this.props.numberOfColors),
+                this.getColorV(i,this.props.numberOfColors)
+                ))}
         </ul>
     }
     getCssRgb(rgb:Array<number>)
@@ -17,9 +29,9 @@ export class Output extends React.PureComponent<OutputState>
         return ['#', ...rgb.map(x=>x.toString(16).padStart(2,'0'))].join('');
 
     }
-    renderColor(deg: number)
+    renderColor(deg: number, sat: number, val: number)
     {
-        var mainRgb = this.hsvToRgb(deg, 1, 1);
+        var mainRgb = this.hsvToRgb(deg, sat, val);
         var rgb = this.getCssRgb(mainRgb);
         return <li>
             <div style={{width: '80px', height:'20px', backgroundColor: rgb}}>
