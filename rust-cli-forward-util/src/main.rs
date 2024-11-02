55600b68259e2fc60483a7077b1775e50825d491
Adilson Jr <dontcallme@adilsonjr.net>
2024-11-02 16:55:32 -0300

    Rust cli tool forwarder

--- rust-cli-forward-util/src/main.rs
+++ rust-cli-forward-util/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process::Command;
+use std::process::ExitStatus;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,11 +9,16 @@ fn main() {
     //     println!("Usage: {} <arg1> [arg2] [arg3] ...", args[0]);
     //     return;
     // }
+    //Writes the folder where this exe is located on in the var exe_path
+    let exe_path = std::env::current_exe().unwrap();
+    let jar_file: &str = "pdfbox-app-3.0.3.jar";
+    
+    //Combine the exe_path and jar_file in jar_full_path variable
+    let jar_full_path = exe_path.parent().unwrap().join(jar_file);
 
-    let jar_file = "pdfbox-app-3.0.3.jar";
-    let status = Command::new("java")
+    let status: ExitStatus = Command::new("java")
         .arg("-jar")
-        .arg(jar_file)
+        .arg(jar_full_path)
         .args(args)
         .status()
         .expect("Failed to start java process");
